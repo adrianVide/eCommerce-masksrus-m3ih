1ef62d5ac5418769b07f997574a2b61b44b356bc
unai gonzalez <unaigoe91@gmail.com>
2020-04-08 17:44:14 +0200

    test

--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Product = require('../models/Product')
 
-/* GET home page. */
+/* GET home page!!. */
 router.get('/', async (req, res, next) => {
   const allProducts = await Product.find()
  res.json(allProducts)

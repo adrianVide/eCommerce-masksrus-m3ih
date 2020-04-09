4365e6dd82781c01994cac5528eee5faa9e4f6fd
unai gonzalez <unaigoe91@gmail.com>
2020-04-09 12:45:41 +0200

    cart and wishlit added

--- server-side/routes/user.js
+++ server-side/routes/user.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/User')
-/* GET users listing. */
-router.get('/wishlist', function(req, res, next) {
 
-User.find()
+/* GET all the user wishlist products */
+router.get('/:id/wishlist', function(req, res, next) {
 
+const {_id} = req.session.currentUser;
+User.findById({_id}).populate('products')
+.then(wishListProds => {
+  res.json(wishListProds.wishList)
+}) 
+.catch(error => {
+  next(error)
+})
+  
 });
+
+// GET all products from cart
+
+router.get('/:id/cart', async (req, res, next) => {
+
+  try {
+const findCartProducts = await User.findById(req.session.currentUser._id).populate('products')
+
+   res.json(findCartProducts.cartList.map(product => product))
+  
+  }catch(err) {
+    res.status(404).json({errorMessage: 'Did not find anything'})
+  }
+
+})
+
 
 module.exports = router;

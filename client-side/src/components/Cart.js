4c62fe6f73b727354a9af66e18a259fd160775af
unai gonzalez <unaigoe91@gmail.com>
2020-04-13 20:15:58 +0200

    client side cart addings

--- client-side/src/components/Cart.js
+++ client-side/src/components/Cart.js
@@ -143,7 +143,7 @@ const Cart = (props) => {
                         <td>Shipping</td>
                         <td class="text-right">6,90 €</td>
                       </tr>
-                      
+
                       <tr>
                         <td></td>
                         <td></td>
@@ -174,16 +174,16 @@ const Cart = (props) => {
               <div class="row">
                 <div class="col-md-3 col-lg-4 col-xl-3">
                   <h5>About</h5>
-                  <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25" />
-                  <p class="mb-0">
-                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+                  <hr class="bg-dark mb-2 mt-0 d-inline-block mx-auto w-25" />
+                  <p style={{color:'black'}} class="mb-0">
+                    Here you will find the best customer service Ever. Trust us and you will no regret it.
                 </p>
                 </div>
                 
                 <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
                   <h5>Others links</h5>
-                  <hr class="bg-danger mb-2 mt-0 d-inline-block mx-auto w-25" />
-                  <ul class="list-unstyled">
+                  <hr class="bg-dark mb-2 mt-0 d-inline-block mx-auto w-25" />
+                  <ul style={{color:'black'}} class="list-unstyled">
                     <li>About us</li>
                     <li>FAQ</li>
                 
@@ -192,12 +192,12 @@ const Cart = (props) => {
 
                 <div class="col-md-4 col-lg-3 col-xl-3">
                   <h5>Contact</h5>
-                  <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25" />
-                  <ul class="list-unstyled">
-                    <li><i class="fa fa-home mr-2"></i> My company</li>
-                    <li><i class="fa fa-envelope mr-2"></i> email@example.com</li>
-                    <li><i class="fa fa-phone mr-2"></i> + 33 12 14 15 16</li>
-                    <li><i class="fa fa-print mr-2"></i> + 33 12 14 15 16</li>
+                  <hr class="bg-dark mb-2 mt-0 d-inline-block mx-auto w-25" />
+                  <ul  style={{color:'black'}} class="list-unstyled">
+                    <li><i class="fa fa-home mr-2"></i> Mask 'R' us</li>
+                    <li><i class="fa fa-envelope mr-2"></i> maskrus@gmail.com</li>
+                    <li><i class="fa fa-phone mr-2"></i> + 93 230 02 21</li>
+                    <li><i class="fa fa-print mr-2"></i> + 669 12 12 43</li>
                   </ul>
                 </div>
                 <div class="col-12 copyright mt-3">

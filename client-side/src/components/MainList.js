eedaa442980e916933ff76a881628608cb9add9d
unai gonzalez <unaigoe91@gmail.com>
2020-04-13 14:05:12 +0200

    fixed things

--- client-side/src/components/MainList.js
+++ client-side/src/components/MainList.js
@@ -1,54 +1,54 @@
 import React from 'react'
 import axios from 'axios'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 const MainList = () => {
-   
-
-    const [data, setData] = useState([]);
-    const [query, setQuery]= useState('')
-
-    useEffect(() => {
-      axios.get(`http://localhost:4000/products`)
-      .then((apiResponse) => {setData(apiResponse.data)})
-    }, [])
 
 
-    const filteredData = data.filter(product => product.name.includes(query)) 
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('')
 
-      return (
-        <div>
-          <form>
-            <input
-              placeholder="Search for..."
-              value={query}
-              onChange={(e) => setQuery(e.target.value) }
-              name='query'
-            />
-          </form>
+  useEffect(() => {
+    axios.get(`http://localhost:4000/products`)
+      .then((apiResponse) => { setData(apiResponse.data) })
+  }, [])
 
-          {filteredData ? (filteredData.map((mask) => {
+
+  const filteredData = data.filter(product => product.name.includes(query))
+
+  return (
+    <div>
+      <form>
+        <input
+          placeholder="Search for..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          name='query'
+        />
+      </form>
+
+      {filteredData ? (filteredData.map((mask) => {
         return (
           <div key={mask._id} className="card border-info mb-1 shadow">
-            
+
             <div className="d-flex card-body text-info">
               <span className="align-self-center">
                 <img className="list-img" src={mask.photo} alt="" />
                 {mask.originalPrice} €
               </span>
               <div><div className="card-header">
-              <h5>{mask.name}</h5>
-            </div>
+                <h5>{mask.name}</h5>
+              </div>
                 {/* <h5 className="card-title">{mask.name}</h5> */}
                 <p className="card-text">{mask.description}</p>
               </div>
             </div>
           </div>
         );
-      })): null }
-          {/* <div>{filteredData ? filteredData.map(product => <p>{product.name}</p>) : null}</div> */}
-        </div>
-      );
-    };
+      })) : null}
+      {/* <div>{filteredData ? filteredData.map(product => <p>{product.name}</p>) : null}</div> */}
+    </div>
+  );
+};
 
-  export default MainList
+export default MainList
--- client-side/src/urls/User.js
+++ client-side/src/urls/User.js
@@ -3,9 +3,10 @@ import { needAuth } from "../lib/Auth-provider";
 
 class User extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
-        <h1>Welcome {this.props.user.username}</h1>
+        <h1>Welcome {this.props.user.email}</h1>
         <button onClick={this.props.logout}>Log out</button>
       </div>
     );
